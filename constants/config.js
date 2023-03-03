d719fc5c94663b91ad9fc9da5d551d35e6b09ef0
kevinrivera <devkevinrivera@gmail.com>
2023-03-03 12:02:58 +0100

    .

--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL = 'https://justlasercut.com' || 'https://justlasercut-prod.vercel.app/';
 export const BASE_URL_MONGO = 'mongodb+srv://kevinriveradev:7h3wEv05Ai5tCcmX@cluster0.a9cm9e8.mongodb.net/justlasercut-stagging?retryWrites=true&w=majority';
-export const BASE_MULTIMEDIA_STORE = "C:/Users/Administrator/Documents/justlasercut/justlasercut/public/";
+export const BASE_MULTIMEDIA_STORE = "C:/Users/Administrator/Documents/justlasercut/justlasercut";
 //export const BASE_MULTIMEDIA_STORE = "C:/Users/steve/Desktop/kevin/justlasercut";
