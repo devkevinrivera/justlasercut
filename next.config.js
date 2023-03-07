bfdf620224080a90c000ce1dba46ba388a7fbf37
Kevin Rivera <devkevinrivera@gmail.com>
2023-03-07 14:19:01 +0100

    .

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@ const path = require("path");
 
 
 module.exports = {
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        "fs": false,
+        "path": false,
+        "os": false,
+        "stream": require.resolve("stream-browserify"),
+        "timers": require.resolve("timers-browserify"),
+        "zlib": false,
+        "crypto": false,
+        "http": false,
+        "dgram": false
+      }
+    }
+    return config
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles/")],
@@ -9,5 +26,6 @@ module.exports = {
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
-  }
+  },
+  
 };
